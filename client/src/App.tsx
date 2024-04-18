8898e3628b848408be80bc8c18dd420ce74ef850
seyoit <seyoit@gmail.com>
2024-04-18 18:57:24 +0900

    ready!

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,7 @@
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hi PlanPeakTeam!</h1>;
+  return (
+    <h1 className="mt-10 text-center text-3xl font-bold underline">
+      Hi PlanPeakTeam!
+    </h1>
+  );
 }
