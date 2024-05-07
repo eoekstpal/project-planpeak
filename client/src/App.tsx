0af71aef22ae6fbb8a1579ec6959f6f2f7661e2a
youngmatias <matiasyoon@gmail.com>
2024-05-07 12:01:40 +0900

    Feat: Add Router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,18 @@
-export default function App() {
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Navigation from './components/Navigation';
+
+function App() {
   return (
-    <></>
+    <>
+      <Router>
+        <Navigation />
+        <Routes>
+          <Route path="/" Component={Home}></Route>
+        </Routes>
+      </Router>{' '}
+    </>
   );
 }
+
+export default App;
