9f2bdcf94595a8654eec5c90763b3199d63f3fe0
inyeong <116771536+marineyeong@users.noreply.github.com>
2024-05-10 13:32:08 +0900

    Feature/container (#11)
    * Rename: "헤더, 푸터, 사이드네비 component -> layouts 폴더 수정"
    
    * Fix: "html 기본 언어 en -> ko 수정"
    
    * Feat: Link 오류 해결을 위한 "BrowserRouter" 추가
    
    * Design: "폰트 간격, 정렬 등 디자인 변경으로 인한 css 수정"
    
    * Feat: "기본 레이아웃 추가"
    
    * Fix: "헤더 link 사용 가능한 a 태그 수정"
    Design: "css 정리"
    
    * Feat: "사이드바가 있는 레이아웃 추가"
    
    * Fix: "시맨틱 태그 활용으로 인한 마크업 및 구조 수정"

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,20 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import Home from './pages/Home';
+import Header from './layouts/Header';
+import Footer from './layouts/Footer';
+import SideNavCont from './layouts/SideNavCont';
 
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" Component={Home}></Route>
-        </Routes>
-      </Router>{' '}
+      <Header />
+      <main>
+        <div className="content">
+          <h1>사이드바 없이 가득찬 레이아웃</h1>
+        </div>
+        <SideNavCont></SideNavCont>
+      </main>
+      <Footer />
     </>
   );
 }
--- client/src/layouts/SideNav.tsx
+++ client/src/layouts/SideNav.tsx
@@ -2,19 +2,17 @@ import { Link } from 'react-router-dom';
 
 function SideNav() {
   return (
-    <div className="py-7 px-5">
-    <ul className="text-ppBlack text-2xl font-bold py-5">마이페이지</ul>
-      <Link to={'/'}>
+    <nav>
+      <ul className="py-7 px-5">
+        <li className="text-ppBlack text-2xl font-bold py-5">마이페이지</li>
         <li className="list-none text-ppGray text-lg font-normal leading-10 hover:text-ppBlue">
-          관리자 프로필 설정
+          <Link to={'/'}>관리자 프로필 설정</Link>
         </li>
-      </Link>
-      <Link to={'/'}>
         <li className="list-none text-ppGray text-lg font-normal leading-10 hover:text-ppBlue">
-          개인정보 변경
+          <Link to={'/'}>개인정보 변경</Link>
         </li>
-      </Link>
-    </div>
+      </ul>
+    </nav>
   );
 }
 
