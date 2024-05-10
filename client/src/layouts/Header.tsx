0c68936e4fc1432e0768430c53f8912ecfa2d571
inyeong <116771536+marineyeong@users.noreply.github.com>
2024-05-10 13:50:03 +0900

    Feature/container (#12)
    * Rename: "헤더, 푸터, 사이드네비 component -> layouts 폴더 수정"
    
    * Fix: "html 기본 언어 en -> ko 수정"
    
    * Feat: Link 오류 해결을 위한 "BrowserRouter" 추가
    
    * Design: "폰트 간격, 정렬 등 디자인 변경으로 인한 css 수정"
    
    * Feat: "기본 레이아웃 추가"
    
    * Fix: "헤더 link 사용 가능한 a 태그 수정"
    Design: "css 정리"
    
    * Feat: "사이드바가 있는 레이아웃 추가"
    
    * Fix: "시맨틱 태그 활용으로 인한 마크업 및 구조 수정"
    
    * Fix: "header 아래 content를 추가하여 최대크기 및 중앙정렬 지정, 관련 css 삭제"

--- client/src/layouts/Header.tsx
+++ client/src/layouts/Header.tsx
@@ -3,57 +3,59 @@ import Logo from '../assets/logo/planpeakHorizontal.jpg';
 
 function Header() {
   return (
-    <header className="flex items-center justify-between py-1">
-      <ul className="flex p-1 items-center space-x-4">
-        <li>
-          <Link to="/">
-            <img style={{ width: '126px' }} src={Logo} alt="logo" />
-          </Link>
-        </li>
-        <li>
-          <Link to="/" className="p-2 text-ppVeryLightGray hover:text-ppBlue">
-            모임 참가하기
-          </Link>
-          <ul className="hidden">
-            <li>
-              <Link to="/" className="p-2 text-ppVeryLightGray hover:text-ppBlue">
-                모든 모임 둘러보기
-              </Link>
-            </li>
-            <li>
-              <Link to="/" className="p-2 text-ppVeryLightGray hover:text-ppBlue">
-                내가 참여중인 모임
-              </Link>
-            </li>
-            <li>
-              <Link to="/" className="p-2 text-ppVeryLightGray hover:text-ppBlue">
-                나의 모임 이력
-              </Link>
-            </li>
-          </ul>
-        </li>
-        <li>
-          <Link to="/" className="p-2 text-ppVeryLightGray hover:text-ppBlue">
-            모임 관리하기
-          </Link>
-        </li>
-        <li>
-          <Link to="/" className="p-2 text-ppVeryLightGray hover:text-ppBlue">
-            플랜픽 소개
-          </Link>
-        </li>
-      </ul>
-      <div className="flex p-2 space-x-4">
-        <span className="overflow-hidden border border-solid border-ppVeryLightGray rounded-full hover:border-transparent hover:bg-ppBlue">
-          <Link to="/" className="block py-2 px-3 text-ppVeryLightGray hover:text-white">
-            로그인
-          </Link>
-        </span>
-        <span className="overflow-hidden border border-solid border-ppVeryLightGray rounded-full hover:border-transparent hover:bg-ppBlue">
-          <Link to="/" className="block py-2 px-3 text-ppVeryLightGray hover:text-white">
-            회원가입하기
-          </Link>
-        </span>
+    <header>
+      <div className="content flex items-center justify-between py-1">
+        <ul className="flex p-1 items-center space-x-4">
+          <li>
+            <Link to="/">
+              <img style={{ width: '126px' }} src={Logo} alt="logo" />
+            </Link>
+          </li>
+          <li>
+            <Link to="/" className="p-2 text-ppVeryLightGray hover:text-ppBlue">
+              모임 참가하기
+            </Link>
+            <ul className="hidden">
+              <li>
+                <Link to="/" className="p-2 text-ppVeryLightGray hover:text-ppBlue">
+                  모든 모임 둘러보기
+                </Link>
+              </li>
+              <li>
+                <Link to="/" className="p-2 text-ppVeryLightGray hover:text-ppBlue">
+                  내가 참여중인 모임
+                </Link>
+              </li>
+              <li>
+                <Link to="/" className="p-2 text-ppVeryLightGray hover:text-ppBlue">
+                  나의 모임 이력
+                </Link>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <Link to="/" className="p-2 text-ppVeryLightGray hover:text-ppBlue">
+              모임 관리하기
+            </Link>
+          </li>
+          <li>
+            <Link to="/" className="p-2 text-ppVeryLightGray hover:text-ppBlue">
+              플랜픽 소개
+            </Link>
+          </li>
+        </ul>
+        <div className="flex p-2 space-x-4">
+          <span className="overflow-hidden border border-solid border-ppVeryLightGray rounded-full hover:border-transparent hover:bg-ppBlue">
+            <Link to="/" className="block py-2 px-3 text-ppVeryLightGray hover:text-white">
+              로그인
+            </Link>
+          </span>
+          <span className="overflow-hidden border border-solid border-ppVeryLightGray rounded-full hover:border-transparent hover:bg-ppBlue">
+            <Link to="/" className="block py-2 px-3 text-ppVeryLightGray hover:text-white">
+              회원가입하기
+            </Link>
+          </span>
+        </div>
       </div>
     </header>
   );
