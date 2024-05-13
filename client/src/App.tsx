ef914d4e1c2f9d7708c558e5e29d1888e42fd0a2
eoekstpal <park.se.mi47@gmail.com>
2024-05-13 17:15:43 +0900

    Rename: "수정 공통 컴포넌트 폴더화, App.tsx 구조 수정"

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,17 +2,13 @@
 // import Home from './pages/Home';
 import Header from './layouts/Header';
 import Footer from './layouts/Footer';
-import SideNavCont from './layouts/SideNavCont';
 
 function App() {
   return (
     <>
       <Header />
-      <main>
-        <div className="content">
-          <h1>사이드바 없이 가득찬 레이아웃</h1>
-        </div>
-        <SideNavCont></SideNavCont>
+      <main className="content">
+        
       </main>
       <Footer />
     </>
