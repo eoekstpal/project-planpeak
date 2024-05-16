cf457930e454a9d9e9c2e3e3657a79755b177cff
HyoJoong, Kim(backdfb) <139917593+backdfb@users.noreply.github.com>
2024-05-16 13:03:58 +0900

    Feature/mainpage (#26)
    * feature/mainpage 중간점검요청
    
    * feature/MainPage 구도잡기 1차 완성
    
    * MainPage 찬영님 카드 반영, dummy data 수록, list와 LandingPage는 완성후 각 브랜치에 push 예정
    
    * landingpage 작성
    
    * LandingPage 메인 글자 상하 간격 수정, 찬영님 Card.tsx 사이즈 일부 수정
    
    * 충돌사항 수정중
    
    * 충돌 수정 2차
    
    * 3차
    
    * landingPage위한 색 변경 완료(Card.tsx 색상 변경)
    
    * mainpage 곧 마감되는 모임 영역 배경색 #d9d9d9로 교체
    
    * list.tsx 작성중
    
    * const -> function으로 수정
    
    * 수정
    
    * list => AllMeetingList.tsx로 변경
    
    * 깃 오류 발생으로 인한 이름 재변경

--- client/src/pages/list.tsx
+++ client/src/pages/list.tsx
@@ -1,9 +1,100 @@
-function List() {
+
+import CardImage from '../components/Common/Card'
+import Input from '../components/forms/Input';
+
+interface Event {
+    imageSrc: string;
+    name: string;
+    date: string;
+    description: string;
+}
+
+const events: Event[] = [
+    {
+        name: '커피 원데이 클래스',
+        description: '커피를 좋아하는 사람들의 모임',
+        date: '을지로, 2024. 4. 29',
+        imageSrc: 'path_to_image',
+    },
+    {
+        name: '드로잉 모임',
+        description: '그림을 그려봐요',
+        date: '을지로, 2024. 4. 29',
+        imageSrc: 'path_to_image',
+    },
+    {
+        name: '영화 평론 모임',
+        description: '영화관람 좋아 하시죠?',
+        date: '을지로, 2024. 4. 29',
+        imageSrc: 'path_to_image',
+    },
+    {
+        name: '문문문',
+        description: '달과 별을 보러 갈까요?',
+        date: '을지로, 2024. 4. 29',
+        imageSrc: 'path_to_image',
+    },
+    {
+        name: '영화연구',
+        description: '영화관람 좋아하세요?',
+        date: '을지로, 2024. 4. 29',
+        imageSrc: 'path_to_image',
+    },
+    {
+        name: '밴드 모임',
+        description: '같이 악기연주 해봐요',
+        date: '을지로, 2024. 4. 29',
+        imageSrc: 'path_to_image',
+    },
+    {
+        name: '영화 평론 모임',
+        description: '영화관람 좋아 하시죠?',
+        date: '을지로, 2024. 4. 29',
+        imageSrc: 'path_to_image',
+    },
+    {
+        name: '문문문',
+        description: '달과 별을 보러 갈까요?',
+        date: '을지로, 2024. 4. 29',
+        imageSrc: 'path_to_image',
+    },
+];
+
+function renderItems(items: Event[])  {
+    return items.map((item) => (
+        <li key={item.name} className="col text-left items-center">
+          <CardImage src={item.imageSrc} alt={item.altText || item.name} />
+          <span className='text-sm text-bold'>{item.name}</span> <br />
+          <span className='text-sm'>{item.description}</span> <br />
+          <span className='text-sm'>{item.date}</span>
+        </li> 
+    ));
+};
+
+
+function List () {
     return (
-        <div>
-            
+      <div>
+        <div className='mt-10 flex items-start ml-60'>
+            <span className="text-ppGray mr-6 font-['Pretendard'] ">
+                모든 모임 둘러보기
+            </span>
+            <Input />
+            <div>
+
+            </div>
         </div>
+        <div>
+            <div>
+                태그검색
+            </div>
+            <div></div>
+        </div>
+      </div>      
     )
 }
 
+
+
 export default List;
+
